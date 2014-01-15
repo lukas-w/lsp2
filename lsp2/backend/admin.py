b844c13f73494ab7403013df360117216253eca9
Tobias Doerffel <tobias.doerffel@gmail.com>
2014-01-15 17:53:50 +0100

    LSP2: initial import

---
+++ lsp2/backend/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from lsp2.backend.models import *
+
+
+admin.site.register(PlatformUser, admin.ModelAdmin)
+admin.site.register(Category, admin.ModelAdmin)
+admin.site.register(Subcategory, admin.ModelAdmin)
+admin.site.register(Comment, admin.ModelAdmin)
+admin.site.register(Rating, admin.ModelAdmin)
+admin.site.register(License, admin.ModelAdmin)
+admin.site.register(FileType, admin.ModelAdmin)
+admin.site.register(File, admin.ModelAdmin)
+
