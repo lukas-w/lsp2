c393255f89d7b1a7d9ab7a94ad3293f1fca33b7c
Lukas Wohlschläger <lukaswhl@gmail.com>
2014-05-17 15:10:00 +0200

    Rename 'Rating' to 'Vote', update settings.py
    * Models: Rename Rating to Vote
    * Update settings.py to Django 1.7 version
    * Minor changes

--- lsp2/backend/models.py
+++ lsp2/backend/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 class PlatformUser(User):
 	loginFailureCount = models.SmallIntegerField(blank=True,null=True)
@@ -44,11 +45,14 @@ class Comment(models.Model):
 	text = models.TextField()
 
 
-class Rating(models.Model):
+class Vote(models.Model):
 	user = models.ForeignKey('PlatformUser', verbose_name=PlatformUser._meta.verbose_name)
 	submission = models.ForeignKey('Submission')
 	date = models.DateTimeField(default=datetime.datetime.now)
 	upvote = models.BooleanField()
+
+	def __str__(self):
+		return '%s: %s1' % (self.submission.name, '+' if self.upvote else '-')
 
 
 class AllowedMime(models.Model):
@@ -86,4 +90,19 @@ class Submission(models.Model):
 	license = models.ForeignKey('License', verbose_name=License._meta.verbose_name, blank=True, null=True)
 	category = models.ForeignKey('Category', verbose_name=Category._meta.verbose_name)
 	subcategory = models.ForeignKey('Subcategory', verbose_name=Subcategory._meta.verbose_name, blank=True, null=True)
+
+	def __str__(self):
+		return '%s "%s"' % (self.filename, self.name)
+
+	def downloadCount(self):
+		return self.file_set.aggregate(Sum('downloadCount'))['downloadCount__sum']
+
+	def upVotes(self):
+		return self.vote_set.filter(upvote = True).count()
+
+	def downVotes(self):
+		return self.vote_set.filter(upvote = False).count()
+
+	def voteBalance(self):
+		return upVotes - downVotes
 
