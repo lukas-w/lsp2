97d516ea04fadb2c1ce8bfcde249067bcf810157
Lukas Wohlschläger <lukaswhl@gmail.com>
2014-05-21 19:30:41 +0200

    Remove Category and Subcategory models
    Category replaced by subclasses, which allows for different fields for each category.
    Subcategory will be replaced by tags.
    
    Other changes:
    * Submission: Move `filename` field to `File`
    * Submission: Rename `user` to `uploader`
    * Submission: Fix `voteBalance()` method
    * Small Python style fixes

--- lsp2/backend/admin.py
+++ lsp2/backend/admin.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from lsp2.backend.models import *
 
 
-admin.site.register(Category, admin.ModelAdmin)
-admin.site.register(Subcategory, admin.ModelAdmin)
 admin.site.register(Comment, admin.ModelAdmin)
 admin.site.register(Vote, admin.ModelAdmin)
 admin.site.register(License, admin.ModelAdmin)
-admin.site.register(AllowedMime, admin.ModelAdmin)
+admin.site.register(Mime, admin.ModelAdmin)
 admin.site.register(File, admin.ModelAdmin)
 admin.site.register(Submission, admin.ModelAdmin)
+admin.site.register(ProjectSubmission, admin.ModelAdmin)
+admin.site.register(SampleSubmission, admin.ModelAdmin)
 
+admin.site.register(ThemeSubmission, admin.ModelAdmin)
--- lsp2/backend/models.py
+++ lsp2/backend/models.py
@@ -4,28 +4,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
 
-class Category(models.Model):
-	name = models.CharField(max_length=32)
-	name_plural = models.CharField(max_length=32)
-
-	def __str__(self):
-		return self.name;
-
-	class Meta:
-		verbose_name_plural = "Categories"
-
-
-class Subcategory(models.Model):
-	name = models.CharField(max_length=64)
-	category = models.ForeignKey('Category', verbose_name=Category._meta.verbose_name)
-
-	class Meta:
-		verbose_name_plural = "Subcategories"
-
 
 class Comment(models.Model):
 	user = models.ForeignKey(User)
-	submission = models.ForeignKey('Submission', verbose_name=Category._meta.verbose_name)
+	submission = models.ForeignKey('Submission')
 	date = models.DateTimeField(default=datetime.datetime.now)
 	text = models.TextField()
 
@@ -33,16 +15,15 @@ class Comment(models.Model):
 class Vote(models.Model):
 	user = models.ForeignKey(User)
 	submission = models.ForeignKey('Submission')
-	date = models.DateTimeField(default=datetime.datetime.now)
+	date = models.DateTimeField(default=datetime.datetime.now, null=True)
 	upvote = models.BooleanField(default=True)
 
 	def __str__(self):
 		return '%s: %s1' % (self.submission.name, '+' if self.upvote else '-')
 
 
-class AllowedMime(models.Model):
-	category = models.ForeignKey('Category', verbose_name=Category._meta.verbose_name)
-	mime = models.CharField(max_length=31)
+class Mime(models.Model):
+	mime = models.CharField(max_length=16)
 
 
 class License(models.Model):
@@ -52,40 +33,50 @@ class License(models.Model):
 
 class File(models.Model):
 	submission = models.ForeignKey('Submission')
+	filename = models.CharField(max_length=128)
 	version = models.PositiveSmallIntegerField()
 	size = models.PositiveSmallIntegerField()
 	downloadCount = models.PositiveSmallIntegerField()
 	sha1hash = models.CharField(max_length=40)
-	
+
 	date = models.DateTimeField(default=datetime.datetime.now)
 
 
 class Submission(models.Model):
 	name = models.CharField(max_length=128)
-	filename = models.CharField(max_length=128)
 	description = models.TextField(blank=True)
 
+	submitDate = models.DateTimeField(default=datetime.datetime.now)
 	updateDate = models.DateTimeField(default=datetime.datetime.now)
 
-	soundcloudId = models.CharField(max_length=255, blank=True)
-
-	user = models.ForeignKey(User)
-	license = models.ForeignKey('License', verbose_name=License._meta.verbose_name, blank=True, null=True)
-	category = models.ForeignKey('Category', verbose_name=Category._meta.verbose_name)
-	subcategory = models.ForeignKey('Subcategory', verbose_name=Subcategory._meta.verbose_name, blank=True, null=True)
+	uploader = models.ForeignKey(User)
+	license = models.ForeignKey(
+		License, verbose_name=License._meta.verbose_name, null=True)
 
 	def __str__(self):
-		return '%s "%s"' % (self.filename, self.name)
+		return self.name
 
 	def downloadCount(self):
 		return self.file_set.aggregate(Sum('downloadCount'))['downloadCount__sum']
 
 	def upVotes(self):
-		return self.vote_set.filter(upvote = True).count()
+		return self.vote_set.filter(upvote=True).count()
 
 	def downVotes(self):
-		return self.vote_set.filter(upvote = False).count()
+		return self.vote_set.filter(upvote=False).count()
 
 	def voteBalance(self):
-		return upVotes - downVotes
+		return self.upVotes() - self.downVotes()
 
+
+class ProjectSubmission(Submission):
+	seconds = models.PositiveSmallIntegerField()
+	soundcloudId = models.CharField(max_length=255, blank=True)
+
+
+class SampleSubmission(Submission):
+	milliseconds = models.PositiveSmallIntegerField()
+
+
+class ThemeSubmission(Submission):
+	pass
